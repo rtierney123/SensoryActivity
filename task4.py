ac61b0f9dfe4094d9c71396458792268bbe6c051
deepak987 <deepakks96@gmail.com>
2019-02-17 19:31:43 -0500

    task 4 - with a fixed threshold

--- task4.py
+++ task4.py
@@ -1,7 +1,7 @@
-import parser
+import parser_data
 from plot_data import plot_data
-
-
+import pandas as pd
+import numpy as np
 def segment_climbing_walking(data):
     '''
     While collecting data on stairs there were times when you were also walking rather than climbing
@@ -19,21 +19,21 @@ def segment_climbing_walking(data):
     return data
 
 
-def count_steps(data):
+def count_steps(walk1):
     print ('count_steps')
     num_steps = 0
-    '''
-    This function counts the number of steps in data and returns the number of steps
-    '''
+    denoised = walk1['BarometerSensor'].diff().values[375:1736]
+    binarized = denoised < -0.008
+    num_steps= np.count_nonzero(binarized)
     return num_steps
 
 
 def main():
     # Get data
-    file_name = "test.csv"  # Change this to your data file name
-    data = parser.get_data(file_name)
-    segmented_data = segment_climbing_walking(data)
-    number_of_steps = count_steps(segmented_data)
+    file_name = "data/climb_steps_2.csv"  # Change this to your data file name
+    walk1 = pd.read_csv(file_name)
+
+    number_of_steps = count_steps(walk1)
     print ("Number of steps counted are :{0:d}".format(number_of_steps))
 
 
